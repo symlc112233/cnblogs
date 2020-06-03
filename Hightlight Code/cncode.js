80d849f3e058c64cc491dfd8b68730b944442508
symlc112233 <66358669+symlc112233@users.noreply.github.com>
2020-06-03 17:53:46 +0800

    Create cncode.js

---
+++ Hightlight Code/cncode.js
@@ -0,0 +1,17 @@
+var part1=
+`
+<!Doctype html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Your Code</title>
+<link type="text/css" rel="stylesheet" href="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/styles/SyntaxHighlighter.css"></link>
+<script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/shBrushXml.js"></script>
+<script language="javascript" src="`
+
+var part2=
+`.js"></script>
+</head>
+<body>
+<textarea name="code" id="code" class="cpp">
+`
