f13444701e7198a023753e6eaa15eecb30b807a9
symlc112233 <66358669+symlc112233@users.noreply.github.com>
2020-06-03 18:06:06 +0800

    Update cncode.js

--- Hightlight Code/cncode.js
+++ Hightlight Code/cncode.js
@@ -6,11 +6,11 @@ var part1=
 <meta charset="UTF-8"/>
 <title>Your Code</title>
 <link type="text/css" rel="stylesheet" href="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/styles/SyntaxHighlighter.css"></link>
-<script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/shBrushXml.js"></script>
+<script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/shCore.js"></script>
 <script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/`
 
 var part2=
-`"></script>
+`.js"></script>
 </head>
 <body>
 <textarea name="code" id="code" class="`
