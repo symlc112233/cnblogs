9cfdee698badc0d71ea329e40ee9e877889e3482
symlc112233 <66358669+symlc112233@users.noreply.github.com>
2020-06-04 12:50:35 +0800

    Update cncode.js

--- Hightlight Code/cncode.js
+++ Hightlight Code/cncode.js
@@ -4,10 +4,10 @@ var part1=
 <html>
 <head>
 <meta charset="UTF-8"/>
-<title>Your Code</title>
-<link type="text/css" rel="stylesheet" href="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/styles/SyntaxHighlighter.css"></link>
-<script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/shCore.js"></script>
-<script language="javascript" src="https://raw.githubusercontent.com/symlc112233/cnblogs/master/Hightlight%20Code/file/scripts/`;
+<title>Highlighted Code</title>
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/symlc112233/cnblogs@v1.0/Hightlight%20Code/file/styles/SyntaxHighlighter.css"></link>
+<script language="javascript" src="https://cdn.jsdelivr.net/gh/symlc112233/cnblogs@v1.0/Hightlight%20Code/file/scripts/shCore.js"></script>
+<script language="javascript" src="https://cdn.jsdelivr.net/gh/symlc112233/cnblogs@v1.0/Hightlight%20Code/file/scripts/`;
 
 var part2=
 `.js"></script>
@@ -24,9 +24,12 @@ var part4=
   dp.SyntaxHighlighter.HighlightAll('code', true,false);
   document.body.removeChild(document.getElementById("code"));
 </script>
+<script language="javascript">
+  window.stop(); //works in all browsers but IE    
+  if ($.browser.msie) {document.execCommand("Stop");};
+</script>
 </body>
 </html>
 `;
 
 var lanDict={"c++":"shBrushCpp","c#":"shBrushCSharp","css":"shBrushCss","delphi":"shBrushDelphi","xml":"shBrushXml","java":"shBrushJava","javascript":"shBrushJScript","php":"shBrushPhp","python":"shBrushPython","ruby":"shBrushRuby","sql":"shBrushSql","vb":"shBrushVb"};
-
